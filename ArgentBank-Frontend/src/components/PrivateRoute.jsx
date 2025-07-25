97ab6f515e4e4134403d36ed4a108fb367086454
Clément Séité <clement.seite06@gmail.com>
2025-07-25 17:05:44 +0200

    Modify private road and swagger file

--- ArgentBank-Frontend/src/components/PrivateRoute.jsx
+++ ArgentBank-Frontend/src/components/PrivateRoute.jsx
@@ -2,7 +2,9 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function PrivateRoute({ children }) {
-  const token = useSelector(state => state.auth.token);
+  const tokenFromRedux = useSelector((state) => state.auth.token);
+  const tokenFromStorage = localStorage.getItem('token') || sessionStorage.getItem('token');
+  const token = tokenFromRedux || tokenFromStorage;
 
   if (!token) {
     return <Navigate to="/sign-in" replace />;
